5c431f4e1d8cf59f6f7d71bbef8cff3ba84157d3
Aryan soni <aryansoni1973@gmail.com>
2024-03-23 21:45:37 +0530

    Implemented Responsive Design and Enhanced DOM Manipulation in UI
    Created a new branch based on the original repository as per the guidelines outlined in The Odin Project course curriculum. This commit focuses on enhancing the user interface by implementing responsive design principles, ensuring optimal viewing experience across various devices and screen sizes. Additionally, applied best practices for DOM manipulation, optimizing the functionality of the application through efficient use of functions. These enhancements aim to improve user interaction and overall usability of the project, aligning it more closely with modern web development standards.

--- script.js
+++ script.js
@@ -1,36 +1,89 @@
-/*LINKING HTML*/
-const rockBtn = document.getElementById('rock');
-const paperBtn = document.getElementById('paper');
-const scissorBtn = document.getElementById('scissor');
-const roundResult = document.getElementById('result');
+// Container & Play again & winner template
+const container = document.querySelector('#container');
+const playAgain = document.querySelector('#play-again');
+const winner = document.querySelector('#winner');
 
-rockBtn.addEventListener("click", function () {
-    playRound("rock");
-})
-paperBtn.addEventListener("click", function () {
-    playRound("paper");
-})
-scissorBtn.addEventListener("click", function () {
-    playRound("scissor");
-})
+// Scores
+const computerScore = document.querySelector('#computer-score');
+const playerScore = document.querySelector('#player-score');
 
-/*RANDOM CHOICE BY COMPUTER*/
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissor"];
-    const rndChoice = Math.floor(Math.random() * choices.length);
-    return result = choices[rndChoice];
+// Buttons
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorBtn = document.querySelector('#scissor');
+const playAgainBtn = document.querySelector('#play-again-btn');
+
+// Choices
+const playerChoice = document.querySelector('#player-choice');
+const computerChoice = document.querySelector('#computer-choice');
+
+// Result
+const finalResult = document.querySelector('#result');
+
+// Player Selection
+let playerSelection;
+let playedRound = 0;
+let totalRound = 5;
+
+// random Computer Choice Function 
+const getComputerChoice = () => {
+    const choice = ['ROCK', 'PAPER', 'SCISSOR'];
+    const rndChoice = Math.floor(Math.random() * choice.length);
+    return choice[rndChoice];
 }
 
-/*PLAYER CHOICE & CONDITIONS*/
-function playRound(playerChoice) {
-    const computerChoice = getComputerChoice();
-    if (playerChoice === computerChoice) {
-        return roundResult.innerHTML = "🧷 It's a Tie ! Both choose : " + playerChoice;
-    } else if ((playerChoice === "rock" && computerChoice === "scissor") ||
-        (playerChoice === "paper" && computerChoice === "rock") ||
-        (playerChoice === "scissor" && computerChoice === "paper")) {
-        return roundResult.innerHTML = "🏆 You Win! " + playerChoice + " beats " + computerChoice;
+// Play Round Function !
+const playRound = () => {
+    const computerSelection = getComputerChoice();
+    computerChoice.textContent = computerSelection;
+    if (playerSelection === computerSelection) {
+        finalResult.textContent = `It's a Tie`;
+    } else if ((playerSelection === "ROCK" && computerSelection === "SCISSOR") ||
+        (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+        (playerSelection === "SCISSOR" && computerSelection === "PAPER")) {
+        finalResult.textContent = 'YOU WIN';
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
     } else {
-        return roundResult.innerHTML = "👏 You Lose! - play again " + computerChoice + " beats " + playerChoice;
+        finalResult.textContent = 'YOU LOSE';
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
+    }
+    playedRound++;
+    if (playedRound === totalRound) {
+        container.style.display = 'none';
+        playAgain.removeAttribute('style');
+        playedRound = 0;
+        if (playerScore.textContent > computerScore.textContent) {
+            winner.textContent = `🎉 You Win with ${playerScore.textContent} points...🎉`
+        } else if (computerScore.textContent > playerScore.textContent) {
+            winner.textContent = `🥹 Computer Wins 👻`
+        } else {
+            winner.textContent = `🤝 It's a Tie 🧣`
+        }
+        playerScore.textContent = 0;
+        computerScore.textContent = 0;
+        finalResult.textContent = 'Play 5 Rounds';
+        computerChoice.textContent = '';
+        playerChoice.textContent = '';
     }
 }
+
+// Event Listeners
+rockBtn.addEventListener('click', () => {
+    playerSelection = 'ROCK';
+    playerChoice.textContent = 'ROCK';
+    playRound();
+});
+paperBtn.addEventListener('click', () => {
+    playerSelection = 'PAPER';
+    playerChoice.textContent = 'PAPER';
+    playRound();
+});
+scissorBtn.addEventListener('click', () => {
+    playerSelection = 'SCISSOR';
+    playerChoice.textContent = 'SCISSOR';
+    playRound();
+});
+playAgainBtn.addEventListener('click', () => {
+    playAgain.style.display = 'none';
+    container.style.display = 'grid';
+})
